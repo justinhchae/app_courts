31efe6ded83d5038602d3ce6ff17d695fbb06262
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-01-03 19:40:06 -0500

    Update writer.py

--- do_data/writer.py
+++ do_data/writer.py
@@ -90,7 +90,7 @@ class Writer():
             filename_pickled = str(filename + pickled)
             path = os.sep.join([self.root, filename_pickled])
 
-            df.to_pickle(path, protocol=5)
+            df.to_pickle(path, protocol=protocol)
 
             if echo:
                 print('Wrote dataframe to', path)
